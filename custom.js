763a3ca21b0b8698f86dda87af0418bad38f63b0
kihaeng <rlgod8227@naver.com>
2023-10-22 14:17:01 +0900

    guide 섹션 추가

--- custom.js
+++ custom.js
@@ -27,11 +27,22 @@ $(function () {
     $(this).parent().siblings().removeClass("active");
   });
 
-  /* Slick Slider - review */
-  $(".review-slider").slick({
+  /* Slick Slider - review and mockup */
+  $(".review-slider, .mockup-slider").slick({
     infinite: true, // 무한반복
     dots: true, // 동그라미 네비게이션 사용안함
     autoplay: true, // 자동 재생
     autoplaySpeed: 2000, // 자동 재생 시 머무는 시간
+  });
+
+  /* Video Modal */
+  $(".open-modal").click(function () {
+    $(".video-modal").fadeIn();
+    $("body").addClass("active");
+  });
+
+  $(".close-modal").click(function () {
+    $(".video-modal").fadeOut();
+    $("body").removeClass("active");
   });
 });
